4609fadc40af2eaf25765622f5f9473114e25425
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-12 17:29:25 -0400

    Weather API framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 
+//GLOBAL VARIABLES
+
 const API_KEY = '0c3d388a798cab0ea9f81255aec739e8';
 
 class State{
-    constructor(shortCode, fullName){
-        this.short = shortCode;
+    constructor(twoCharCode, fullName){
+        this.short = twoCharCode;
         this.full = fullName;
     }
 }
-
 const US_STATES = [
     new State('AK', 'Alaska'),
     new State('AL', 'Alabama'),
@@ -68,10 +69,57 @@ const US_STATES = [
     new State('WY', 'Wyoming'),
 ];
 
+const SYSTEM_ERROR_EL = $('<h4>')
+    .addClass('error-msg text-center')
+    .text('System error—please try again');
+
+
+//FUNCTIONS
+
+//True if US, false if int'l
+function US(){
+    return !(document.querySelector('#us-intl-toggler').checked);
+}
+
+
+// Add a US country code, or format the int'l country code correctly for the API, if necessary
+function formatCountryCode(searchTerm){
+    var searchWords = searchTerm.toLowerCase().split(/[,\s]+/); //splits based on this RegEx (w/ either commas or spaces as delimiters
+    var output = '';
+    
+    if (US()){
+        var removeDupStateCode = false;
+
+        var stateCodes = '';
+        US_STATES.forEach(state => {
+            stateCodes += state.short.toLowerCase() + ' ';
+        });
+
+        searchWords.forEach(elem => {
+            if(stateCodes.includes(elem)){
+                output += ', ' + elem + ', us';
+                removeDupStateCode = true;
+            }
+            else if (!(elem === 'us' && removeDupStateCode))
+                output += ' ' + elem;
+        });
+    }
+    else{ //int'l
+        searchWords.forEach(elem => {
+            if (elem.length === 2)
+                output += ', ' + elem;
+            else
+                output += ' ' + elem;
+        });
+    }
+
+    return output.trim();
+}
+
 
 // Handle if multiple possible cities match the search term
 function cityOptions(searchTerm){
-    $('#city-options-wrapper').empty();
+    $('#city-options-wrapper').empty().removeClass('d-none');
     
     // Geocoding API
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchTerm + '&limit=5&appid=' + API_KEY)
@@ -91,24 +139,13 @@ function cityOptions(searchTerm){
                             .attr('id', 'city-options-btns-wrapper')
                             .addClass('row justify-content-center'));
 
-                        filteredData.forEach(option => {
-                            var btnText = [option.name];
-                            if (option.state){
-                                if (US())
-                                    btnText.push(US_STATES.find(state => state.full === option.state).short);    
-                                else //int'l
-                                    btnText.push(option.state);
-                                    
-                            }
-                            if (!US()) //add country code for int'l only
-                                btnText.push(option.country);
-                            
+                        filteredData.forEach(option => 
                             $('#city-options-btns-wrapper').append($('<button>')
                                 .addClass('city-options-btn')
                                 .attr('lat', option.lat)
                                 .attr('lon', option.lon)
-                                .text(btnText.join(', ')));
-                        });
+                                .text(getCityText(option)))
+                        );
 
                         $('#city-options-wrapper').append($('<h4>')
                             .attr('id', 'city-options-footer')
@@ -116,68 +153,28 @@ function cityOptions(searchTerm){
                             .text("If none of these options fits, try making your search more specific"));
 
                         $('#city-options-wrapper').on('click', '.city-options-btn', function(){
-                            //SEARCH BASED ON LAT AND LON
+                            getWeather($(this).attr('lat'), $(this).attr('lon'), $(this).text())
                         });
                     }
                     
                     // If the search term yields only one city option
-                    else if (filteredData.length === 1){
-                        console.log('only one result found!');
-                        //SEARCH BASED ON LAT AND LON
-                    }
+                    else if (filteredData.length === 1)
+                        getWeather(filteredData[0].lat, filteredData[0].lon, getCityText(filteredData[0]));
 
                     // If the search terms doesn't yield any results
-                    else {
+                    else 
                         $('#city-options-wrapper').append($('<h4>')
                             .addClass('error-msg text-center')
                             .text('No results found'));
-                    }
                 })
             } else
                 throw '';
 
         // If the geocoding API fails
         }).catch(error => {
-            $('#city-options-wrapper').append($('<h4>')
-                .addClass('error-msg text-center')
-                .text('There was a system error with your search—please try again'));
+            $('#city-options-wrapper').append(SYSTEM_ERROR_EL);
         });
 };
-
-
-// Add country code to the search term if necessary
-function addCountryCode(searchTerm){
-    var searchWords = searchTerm.toLowerCase().split(/[,\s]+/); //splits based on this RegEx (w/ either commas or spaces as delimiters
-    var output = '';
-    
-    if (US()){
-        var removeDuplicateStateCode = false;
-
-        var stateCodes = '';
-        US_STATES.forEach(state => {
-            stateCodes += state.short.toLowerCase() + ' ';
-        });
-
-        searchWords.forEach(elem => {
-            if(stateCodes.includes(elem)){
-                output += ', ' + elem + ', us';
-                removeDuplicateStateCode = true;
-            }
-            else if (!(elem === 'us' && removeDuplicateStateCode))
-                output += ' ' + elem;
-        });
-    }
-    else{ //int'l
-        searchWords.forEach(elem => {
-            if (elem.length === 2)
-                output += ', ' + elem;
-            else
-                output += ' ' + elem;
-        });
-    }
-
-    return output.trim();
-}
 
 
 // Filter returned city options based on US vs. Int'l
@@ -197,10 +194,48 @@ function filterOptionsUSIntl(options){
 }
 
 
-function US(){
-    return !($('#us-intl-toggler').is(':checked'));
+// Get full text for City(, State / ST)(, Country)
+function getCityText(dataEl){
+    var output = [dataEl.name];
+
+    if (dataEl.state){
+        if (US())
+            output.push(US_STATES.find(state => state.full === dataEl.state).short);    
+        else //int'l
+            output.push(dataEl.state);       
+    }
+
+    if (!US()) //add country code for int'l only
+        output.push(dataEl.country);
+    
+    return output.join(', ');
 }
 
+
+
+// Get and display weather data for given city
+function getWeather(lat, lon, text){
+    console.log('lat: ' + lat, 'lon: ' + lon, 'text: ' + text);
+
+    $('#city-options-wrapper').addClass('d-none');
+
+    //Weather API
+    fetch('http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appID=' + API_KEY)
+        .then(response => {
+            if (response.ok){
+                response.json().then(data => {
+                    console.log(data);
+                })
+            } else
+                throw '';
+        }).catch(error => {
+            $('#weather-now-wrapper').append(SYSTEM_ERROR_EL);
+        });
+}
+
+
+
+//LISTENERS
 
 // toggle "US" vs "Int'l"
 $('#us-intl-toggler').change(function(){
@@ -222,7 +257,7 @@ $('#search-form').submit(function(event){
     event.preventDefault();
 
     var searchTerm = $('#city-input').val().trim();
-    searchTerm = addCountryCode(searchTerm);
+    searchTerm = formatCountryCode(searchTerm);
 
     cityOptions(searchTerm);
 });
